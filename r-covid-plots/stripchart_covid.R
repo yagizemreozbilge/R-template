6682a47f56f5a24f4ea3f9ab48a10a1d800c04c0
yagizemreozbilge <yemre.ozbilge@gmail.com>
2025-04-20 13:22:42 +0300

    moved images in images files

--- r-covid-plots/stripchart_covid.R
+++ r-covid-plots/stripchart_covid.R
@@ -1,5 +1,16 @@
-stripchart(new_cases ~ location, data=subset(data, location %in% c("Turkey", "Italy", "Germany")),
-           method="jitter", pch=16, col=c("orange", "blue", "green"),
-           main="New Cases Strip Chart", xlab="Country", ylab="New Cases")
+
+# Create a subset with only the selected countries
+selected_countries <- c("Turkey", "Germany", "India", "United Kingdom", "United States")
+subset_data <- subset(data, location %in% selected_countries)
+
+# Strip Chart for new_cases by country
+stripchart(new_cases ~ location,
+           data = subset_data,
+           method = "jitter",       # spread points for visibility
+           pch = 16,                # solid circle
+           col = c("orange", "blue", "green", "purple", "red"),
+           main = "New Cases Strip Chart (Selected Countries)",
+           xlab = "Country", ylab = "New Cases")
+
 
 
